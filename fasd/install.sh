ea3cab7d46c242546192f4fd3acdce46d2ed9fe6
fogine <fogine@opmbx.org>
2016-03-28 08:21:33 -0400

    - do not recursive search for install.sh scripts... - add `fasd` dependency and install.sh script

---
+++ fasd/install.sh
@@ -0,0 +1,6 @@
+cd $(dirname $0)
+export PREFIX="~/.local"
+
+echo "-------------------INSTALLING 'fasd'------------------"
+make install
+echo "-----------------------END 'fasd'---------------------"
