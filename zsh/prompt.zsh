30da242ba645d984971582c5bde667cac039eeda
fogine <fogine@opmbx.org>
2017-03-10 00:39:36 +0100

    map 'jj' key sequence goes to vi command mode in zsh, adds powerline configuration

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,17 +1,8 @@
-#User powerline plugin in shell
-function _update_ps1()
-{
-    error=$?
-    if [[ -s "/usr/local/bin/pypy" ]]; then
-        export PROMPT="$(pypy ~/.oh-my-zsh/plugins/powerline-zsh/powerline-zsh.py $error)"
-    else
-        export PROMPT="$(~/.oh-my-zsh/plugins/powerline-zsh/powerline-zsh.py  $error)"
-    fi
-}
-precmd()
-{
-    _update_ps1
-}
+#Use powerline plugin in shell
+powerline-daemon -q
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+. /usr/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
 
 ## workaround for handling TERM variable in multiple tmux sessions properly from http://sourceforge.net/p/tmux/mailman/message/32751663/ by Nicholas Marriott
 if [[ -n ${TMUX} && -n ${commands[tmux]} ]];then
@@ -23,7 +14,12 @@ if [[ -n ${TMUX} && -n ${commands[tmux]} ]];then
                         TERM=screen
         esac
 fi
+
 #<c-z> - toggle between bg and fg a current process
 _zsh_cli_fg() { fg;}
 zle -N _zsh_cli_fg
 bindkey '^Z' _zsh_cli_fg
+
+
+# 'jj' goes to command mode
+bindkey -M viins 'jj' vi-cmd-mode
