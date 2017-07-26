4b9e73ac538ba0e37bdba638c325b023058c9b12
fogine <fogine@opmbx.org>
2017-07-26 14:38:59 +0200

    aliases for reloading various configs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,5 @@
-alias reload!='. ~/.zshrc'
+alias reload_zshrc!='. ~/.zshrc'
+alias reload_i3!='i3-msg restart'
+alias reload_tmux!='tmux source-file ~/.tmux.conf'
+alias reload_Xresources!='xrdb ~/.Xresources'
+alias reload_fonts!='fc-cache -vf ~/.fonts'
