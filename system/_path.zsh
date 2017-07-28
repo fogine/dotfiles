6a805fb291ba1c7c753fcd25e0d5172889da167a
fogine <fogine@opmbx.org>
2017-07-28 19:15:29 +0200

    clear path env var

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="$HOME.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$ZSH/bin:$PATH"
+export PATH="ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
