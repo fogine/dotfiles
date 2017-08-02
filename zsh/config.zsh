603993cef8a5ca6ca28451fde0c0c37fbc57c6e5
fogine <fogine@opmbx.org>
2017-08-03 00:43:25 +0200

    gpg-agent should use pinentry-tty in interactive zsh shell, otherwise it should use pinentry-gtk-2

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -11,6 +11,8 @@ export GPG_TTY=$(tty)
 # Refresh gpg-agent tty in case user switches into an X session
 gpg-connect-agent updatestartuptty /bye >/dev/null
 export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
+# required by cutom "smart-pinentry" script
+export PINENTRY_USER_DATA=tty
 
 fpath=($ZSH/functions $fpath)
 
