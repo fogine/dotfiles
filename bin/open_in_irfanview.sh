8b4a068fddb0bf495fc2457448b2a02b668aeef2
fogine <7884288+fogine@users.noreply.github.com>
2019-02-24 22:02:54 +0100

    cleanup

--- bin/open_in_irfanview.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#export PS4="\$LINENO: "     # Prints out the line number being executed by debug
-#set -xv                     # Turn on debugging
-wine "$HOME/.local/share/wineprefixes/irfanview/drive_c/Program Files (x86)/IrfanView/i_view32.exe" "Z:\\$(echo "$@" | sed 's/\//\\/g')"
-#set +xv
-
